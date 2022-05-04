45560d4909bc2c6e36f1da9d9f8867c2c8ddc8eb
Adiks <apacholak97@interia.pl>
2022-05-04 10:30:11 +0200

    completing assignment

---
+++ src/main/java/com/adrian/jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.adrian.jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
